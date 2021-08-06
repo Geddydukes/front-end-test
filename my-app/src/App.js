f378df184c12cddf0833fca1a581c3a00f8cdbaa
gdukes707 <Gdukes707@gmail.com>
2021-08-06 16:23:35 -0700

    sixth commit - webpack issue figured out

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import Header from "./components/Header";
+import Hero from "./components/Hero";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Hero />
       <Body />
       <Footer />
     </div>
