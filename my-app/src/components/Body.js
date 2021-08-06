8a97e24516807cf4f6b127bf4a98664264eeca3a
gdukes707 <Gdukes707@gmail.com>
2021-08-06 16:35:08 -0700

    seventh commit - body text added

--- my-app/src/components/Body.js
+++ my-app/src/components/Body.js
@@ -3,11 +3,28 @@ import React from "react";
 const Body = () => {
   return (
     <div className="wholeBody">
-      <div className="bodySectionOne"></div>
-      <div className="bodySectionTwo"></div>
-      <div className="bodySectionThree"></div>
-      <div className="bodySectionFour"></div>
-      <div className="bodySectionFive"></div>
+      <div className="bodySectionOne">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facilis
+        consequatur veritatis deserunt temporibus non ducimus officiis
+        repudiandae? A porro maiores soluta cumque voluptas! Adipisci minus
+        facere quod quos ut.
+      </div>
+      <div className="bodySectionTwo">
+        lorem ipsum <button>Button Button</button>
+      </div>
+      <div className="bodySectionThree">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        adipisci quos tenetur recusandae quis fugiat exercitationem autem
+        nesciunt odit labore velit voluptatem, voluptates id fuga ratione cum.
+        Dignissimos, amet facere.
+      </div>
+      <div className="bodySectionFour">
+        <header>LOREM IPSUM</header>Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. In numquam mollitia atque consequatur vero. Quo, minus
+        tempora in porro illo inventore, nam quos provident veritatis cupiditate
+        aliquid beatae nesciunt veniam. <button>Button Button</button>
+      </div>
+      <div className="bodySectionFive">Lorem ipsum dolor sit amet</div>
     </div>
   );
 };
