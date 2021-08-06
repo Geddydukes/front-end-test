3846b8c77932b5a4ca2fe12722f89bf0b70c3b42
gdukes707 <Gdukes707@gmail.com>
2021-08-06 16:40:37 -0700

    eigth commit - added carosel and icon components

---
+++ my-app/src/components/Carosel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Carosel = () => {
+  return <div className="Carosel"></div>;
+};
+
+export default Carosel;
---
+++ my-app/src/components/Icons.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Icons = () => {
+  return (
+    <div className="Icons">
+      <a href="https://www.facebook.com/gdukes707" target="blank">
+        <i class="fa fa-facebook" aria-hidden="true"></i>
+      </a>
+      <a href="https://www.instagram.com/geddydukes/" target="blank">
+        <i class="fa fa-instagram" aria-hidden="true"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Icons;
