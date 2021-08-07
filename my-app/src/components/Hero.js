e0413d95624345b9cdbe09f15e1e550cebdda6bd
gdukes707 <Gdukes707@gmail.com>
2021-08-06 17:58:31 -0700

    tenth commit - hero image background at currently acceptable rate

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -3,11 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <div className="heroContainer">
-      <header className="heroHeader">Rooster Grin</header>
-      <div className="heroTextContainer">
-        <p className="heroWelcome">Welcome To</p>
-        <p className="heroText">Rooter Grin</p>
-        <button className="heroButton">Button Button</button>
+      <div className="heroImage">
+        <header className="heroHeader">Rooster Grin</header>
+        <div className="heroTextContainer">
+          <p className="heroWelcome">Welcome To</p>
+          <p className="heroText">Rooter Grin</p>
+          <button className="heroButton">Button Button</button>
+        </div>
       </div>
     </div>
   );
